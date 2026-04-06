179476297fa7f90f7dc6793ebaa99c1a3332046c
damilolaadefenwa <ade24039@byui.edu>
2026-04-06 17:51:34 +0100

    fourth commit

--- lesson01/src/App.jsx
+++ lesson01/src/App.jsx
@@ -1,5 +1,6 @@
-import './App.css'
 import Header from './Header'
+import Content from './content'
+import Footer from './Footer'
 
 function App() {
 
@@ -7,6 +8,8 @@ function App() {
     <>
       <div className='App' >
         <Header />
+        <Content />
+        <Footer />
         
       </div>
     </>
--- lesson01/src/Header.jsx
+++ lesson01/src/Header.jsx
@@ -1,7 +1,8 @@
 
 const Header = () => {
+
   return (
-      <header>
+    <header>
           <h1>Groceries List</h1>
     </header>
   )
