7e7e1daa89d5e2ecaddb44ef6a686d6c8b718991
damilolaadefenwa <ade24039@byui.edu>
2026-04-14 14:25:27 +0100

    learning React commit

---
+++ lesson01/src/ReactRouter.jsx
@@ -0,0 +1,14 @@
+/* LEARNING REACT ROUTER FOR FULL APPLICATION */
+
+
+function ReactRouter() {
+    return (
+        <div className='App'>
+            <header>
+                <h1>LEARN REACT</h1>
+            </header>
+        </div>
+    )
+}
+
+export default ReactRouter
--- lesson01/src/main.jsx
+++ lesson01/src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+/* import './index.css' */
+import './reactRouter.css'
+/* import App from './App.jsx' */
+import ReactRouter from './ReactRouter.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ReactRouter />
   </StrictMode>,
 )
