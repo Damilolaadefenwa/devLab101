015bf2a6c1160f731f102b2cf0c168e3d1497c44
damilolaadefenwa <ade24039@byui.edu>
2026-04-16 17:36:48 +0100

    last commits for today

--- lesson01/src/About.jsx
+++ lesson01/src/About.jsx
@@ -1,8 +1,9 @@
 
 const About = () => {
   return (
-        <main>
-            About
+       <main className='About'>
+            <h2>About</h2>
+            <p style={{ marginTop: "1rem" }}>This blog app is a project in the Learn React tutorial series.</p>
         </main>
     )
 }
--- lesson01/src/ReactRouter.jsx
+++ lesson01/src/ReactRouter.jsx
@@ -9,6 +9,7 @@ import About from './About.jsx';
 import Missing from './Missing.jsx';
 import { Route, Switch, useHistory } from 'react-router-dom';
 import { useState, useEffect } from 'react';
+import { format } from 'date-fns';
 
 
 function ReactRouter() {
@@ -44,10 +45,25 @@ function ReactRouter() {
     const [postBody, setPostBody] = useState('');
     const history = useHistory();
 
+    useEffect(() => {
+        const filteredResults = posts.filter((post) =>
+            ((post.body).toLowerCase()).includes(search.toLowerCase())
+            || ((post.title).toLowerCase()).includes(search.toLowerCase()));
+
+            setSearchResults(filteredResults.reverse());
+    }, [posts, search])
+
+
     const handleSubmit = (e) => {
         e.preventDefault();
         const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
-        const datetime = '';   
+        const datetime = format(new Date(), 'MMMM dd', 'yyyy pp');
+        const newPost = { id, title: postTitle, datetime, body: postBody };
+        const allPosts = [...posts, newPost];
+        setPosts(allPosts);
+        setPostTitle('');
+        setPostBody('');
+        history.push('/');
     }
 
     const handleDelete = (id) => {
@@ -62,7 +78,8 @@ function ReactRouter() {
             <Nav search={search} setSearch={setSearch} />
             <Switch>
                 <Route exact path='/'>
-                    <Home posts={posts} />
+                    <Home posts={searchResults} />
+                    {/* <Home posts={posts} /> */}
                 </Route>
                 <Route exact path='/post'>
                     <NewPost
