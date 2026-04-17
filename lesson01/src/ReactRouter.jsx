5d1c788776251c0c928aea21ca98b94910412f4d
damilolaadefenwa <ade24039@byui.edu>
2026-04-17 19:11:34 +0100

    latest commit

--- lesson01/src/ReactRouter.jsx
+++ lesson01/src/ReactRouter.jsx
@@ -10,40 +10,37 @@ import Missing from './Missing.jsx';
 import { Route, Switch, useHistory } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import { format } from 'date-fns';
+import api from './api/posts.jsx';
 
 
 function ReactRouter() {
-     const [posts, setPosts] = useState([
-    {
-      id: 1,
-      title: "My First Post",
-      datetime: "July 01, 2021 11:17:36 AM",
-      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
-    },
-    {
-      id: 2,
-      title: "My 2nd Post",
-      datetime: "July 01, 2021 11:17:36 AM",
-      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
-    },
-    {
-      id: 3,
-      title: "My 3rd Post",
-      datetime: "July 01, 2021 11:17:36 AM",
-      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
-    },
-    {
-      id: 4,
-      title: "My Fourth Post",
-      datetime: "July 01, 2021 11:17:36 AM",
-      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
-    }
-  ])
+    const [posts, setPosts] = useState([]);
     const [search, setSearch ] = useState('');
     const [searchResults, setSearchResults] = useState([]);
     const [postTitle, setPostTitle] = useState('');
     const [postBody, setPostBody] = useState('');
     const history = useHistory();
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+        try {
+            const response = await api.get('/posts');
+            setPosts(response.data);
+        } catch (err) {
+            if (err.response) {
+            // Not in the 200 response range 
+            console.log(err.response.data);
+            console.log(err.response.status);
+            console.log(err.response.headers);
+            } else {
+            console.log(`Error: ${err.message}`);
+            }
+        }
+        }
+
+        fetchPosts();
+    }, [])
+
 
     useEffect(() => {
         const filteredResults = posts.filter((post) =>
@@ -54,17 +51,23 @@ function ReactRouter() {
     }, [posts, search])
 
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
-        const datetime = format(new Date(), 'MMMM dd', 'yyyy pp');
+        const id = posts.length ? String(Number(posts[posts.length - 1].id) + 1) : "1";
+        const datetime = format(new Date(), 'MMMM dd, yyyy pp');
         const newPost = { id, title: postTitle, datetime, body: postBody };
-        const allPosts = [...posts, newPost];
-        setPosts(allPosts);
-        setPostTitle('');
-        setPostBody('');
-        history.push('/');
+        try {
+            const response = await api.post('/posts', newPost);
+            const allPosts = [...posts, response.data];
+            setPosts(allPosts);
+            setPostTitle('');
+            setPostBody('');
+            history.push('/');
+        } catch (err) {
+            console.log(`Error: ${err.message}`); 
+        }
     }
+
 
     const handleDelete = (id) => {
         const postsList = posts.filter(post => post.id !== id);
